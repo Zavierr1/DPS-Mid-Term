e839fba24d82f446062345b36e6d5e66e60d51f0
Fier <muhammad.jafier@student.president.ac.id>
2025-07-01 20:30:43 +0700

    add doccker file and change the file structure to backend and frontend

--- frontend/config/supabaseClient.js
+++ frontend/config/supabaseClient.js
@@ -4,4 +4,4 @@ const supabaseUrl = import.meta.env.VITE_APP_URL;
 const supabaseKey = import.meta.env.VITE_APP_ANON_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
-export default supabase
+export default supabase;
