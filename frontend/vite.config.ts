4f06141f7e87eb423118330dbd8353ceeb6f7453
Fier <muhammad.jafier@student.president.ac.id>
2025-07-07 10:55:45 +0700

    add vitt config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,15 +3,16 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
+export default defineConfig({  plugins: [
     react(), 
     tailwindcss()
   ],
   server: {
+    host: '0.0.0.0',
+    port: 5173,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'http://backend:5000',
         changeOrigin: true,
         secure: false,
       }
